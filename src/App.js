06dea4304a9735b607e4fc50d0ff782ac545be7c
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-11-07 20:46:39 -0800

    added main, footer js

--- src/App.js
+++ src/App.js
@@ -26,28 +26,18 @@
 
 import React from 'react';
 import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 import HornedBeast from './HornedBeast.js';
-// import Main from './Main.js'
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
-        <main>
-          <HornedBeast
-            title="Horn1"
-          hornShape="Spiral"
-          common="RARE"/>
-          <HornedBeast title="Horn2"
-            hornShape="Spike"
-            common="Common" />
-          <HornedBeast title="Horn3"
-            hornShape="Curved"
-            common="Uncommon" />
-        </main>
-        <footer>&copy; Aubrey Corsetti 2022</footer>
+        <Header/>
+        <Footer/>
+        <Main/>
       </>);
   }
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <footer>
+          <p> &copy; Aubrey Corsetti 2022</p>
+        </footer>
+    )
+  }
+
+}
+
+export default Footer;
